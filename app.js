e4fbe93614e1ab40d621839e9e62855b056649c4
nicolassg41 <92997063+nicolassg41@users.noreply.github.com>
2024-06-16 23:16:25 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('https://api.spacexdata.com/v4/launches')
+        .then(response => response.json())
+        .then(data => {
+            const apiData = document.getElementById('apiData');
+            data.forEach(launch => {
+                const launchElement = document.createElement('div');
+                launchElement.classList.add('card', 'mb-3');
+                launchElement.innerHTML = `
+                    <div class="card-body">
+                        <h5 class="card-title">${launch.name}</h5>
+                        <p class="card-text">Fecha de lanzamiento: ${new Date(launch.date_utc).toLocaleDateString()}</p>
+                        <p class="card-text">Lugar: ${launch.launchpad}</p>
+                    </div>
+                `;
+                apiData.appendChild(launchElement);
+            });
+        })
+        .catch(error => {
+            const messageDiv = document.getElementById('message');
+            messageDiv.innerHTML = `<div class="alert alert-danger" role="alert">Error al cargar los datos de la API: ${error.message}</div>`;
+        });
+});
